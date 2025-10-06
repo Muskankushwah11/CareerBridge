81e5c166376b695e20f84f9793f495dd02f9f3d7
Muskan <muskankushwah048@gmail.com>
2025-10-07 01:21:53 +0530

    vercel.json

--- backend/index.js
+++ backend/index.js
@@ -33,12 +33,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
  app.use("/api/v1/application", applicationRoute);
 
- app.get('/',(req,res) => {
-    res.send({
-        activeStatus:true,
-        error:false,
-    })
- })
+ app.get("/", (req, res) => {
+  res.send("Career Bridge Backend is running 🚀");
+});
+
 
 
 
